d0d5a10fa727dba370805324ee02c8cb2863acea
AbhinavNair0077 <159948606+AbhinavNair0077@users.noreply.github.com>
2024-03-13 15:29:27 +0530

    aao push kare

--- login_validation.js
+++ login_validation.js
@@ -1,3 +1,5 @@
+let captcha;
+var firstName = document.getElementById('user-input');
 function formValidation() {
     var uid = document.getElementById('username');
     var passid = document.getElementById('password');
@@ -10,8 +12,10 @@ function formValidation() {
            {
                 if(confirm_pass(confirmPassword, passid))
                 {
-                    window.location.reload();
-                    return true;
+                    if (printmsg() == true){
+                        window.location.reload();
+                        return true;
+                    }
                 }
             }
         }
@@ -20,7 +24,49 @@ function formValidation() {
     return false;
 }
 
+function printmsg() {
+	const usr_input = document
+		.getElementById("submit").value;
 
+	// Check whether the input is equal
+	// to generated captcha or not
+	if (usr_input == captcha.innerHTML) {
+		let s = document.getElementById("key")
+			.innerHTML = "Matched";
+            window.location.href = 'login.html';
+		generate();
+	}
+	else {
+		let s = document.getElementById("key")
+			.innerHTML = "not Matched";
+            alert("Captcha does not match",window.location.href='register.html');
+		generate();
+        return false;
+	}
+}
+function generate() {
+
+	// Clear old input
+	document.getElementById("submit").value = "";
+
+	// Access the element to store
+	// the generated captcha
+	captcha = document.getElementById("image");
+	let uniquechar = "";
+
+	const randomchar =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	// Generate captcha for length of
+	// 5 with random character
+	for (let i = 1; i < 5; i++) {
+		uniquechar += randomchar.charAt(
+			Math.random() * randomchar.length)
+	}
+
+	// Store generated input
+	captcha.innerHTML = uniquechar;
+}
 function userid_validation(uid, mx, my) {
     var uid_len = uid.value.length;
     if (uid_len == 0 || uid_len >= my || uid_len < mx) {
