cfb9184058101672000609eb12441975217f8690
AbhinavNair0077 <159948606+AbhinavNair0077@users.noreply.github.com>
2024-03-20 00:41:10 +0530

    login session

--- validation.js
+++ validation.js
@@ -22,12 +22,14 @@ function formValidation() {
                         if (ageCheck(age)) {
                             if (contactCheck(contact)) {
                                 if (passid_validation(passid, 1, Infinity)) {
-                                    if(confirm_pass(confirmPassword, passid)){
-                                        if (printmsg() == true){
-                                            window.location.reload();
+                                        window.location.reload();
                                             return true;
-                                        }
-                                    }
+                                    // if(confirm_pass(confirmPassword, passid)){
+                                    //     // if (printmsg() != true){
+                                    //     //     window.location.reload();
+                                    //     //     return true;
+                                    //     // }
+                                    // }
                                 }
                             }
                         }
@@ -39,56 +41,14 @@ function formValidation() {
     return false;
 }
 
-function generate() {
 
-	// Clear old input
-	document.getElementById("submit").value = "";
-
-	// Access the element to store
-	// the generated captcha
-	captcha = document.getElementById("image");
-	let uniquechar = "";
-
-	const randomchar =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-	// Generate captcha for length of
-	// 5 with random character
-	for (let i = 1; i < 5; i++) {
-		uniquechar += randomchar.charAt(
-			Math.random() * randomchar.length)
-	}
-
-	// Store generated input
-	captcha.innerHTML = uniquechar;
-}
-
-function printmsg() {
-	const usr_input = document
-		.getElementById("submit").value;
-
-	// Check whether the input is equal
-	// to generated captcha or not
-	if (usr_input == captcha.innerHTML) {
-		let s = document.getElementById("key")
-			.innerHTML = "Matched";
-            window.location.href = 'login.html';
-		generate();
-	}
-	else {
-		let s = document.getElementById("key")
-			.innerHTML = "not Matched";
-            alert("Captcha does not match",window.location.href='register.html');
-		generate();
-        return false;
-	}
-}
 
 function userid_validation(uid, mx, my) {
     var uid_len = uid.value.length;
     if (uid_len == 0 || uid_len >= my || uid_len < mx) {
         alert("User Id should not be empty / length be between " + mx + " to " + my);
         uid.focus();
+        setcustomin
         return false;
     }
     return true;
@@ -185,3 +145,54 @@ function ValidateEmail(uemail) {
         return false;
     }
 }
+
+
+
+
+
+
+
+// function generate() {
+
+// 	// Clear old input
+// 	document.getElementById("submit").value = "";
+
+// 	// Access the element to store
+// 	// the generated captcha
+// 	captcha = document.getElementById("image");
+// 	let uniquechar = "";
+
+// 	const randomchar =
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+// 	// Generate captcha for length of
+// 	// 5 with random character
+// 	for (let i = 1; i < 5; i++) {
+// 		uniquechar += randomchar.charAt(
+// 			Math.random() * randomchar.length)
+// 	}
+
+// 	// Store generated input
+// 	captcha.innerHTML = uniquechar;
+// }
+
+// function printmsg() {
+// 	const usr_input = document
+// 		.getElementById("submit").value;
+
+// 	// Check whether the input is equal
+// 	// to generated captcha or not
+// 	if (usr_input == captcha.innerHTML) {
+// 		let s = document.getElementById("key")
+// 			.innerHTML = "Matched";
+//             window.location.href = 'login.html';
+// 		generate();
+// 	}
+// 	else {
+// 		let s = document.getElementById("key")
+// 			.innerHTML = "not Matched";
+//             alert("Captcha does not match",window.location.href='register.html');
+// 		generate();
+//         return false;
+// 	}
+// }
