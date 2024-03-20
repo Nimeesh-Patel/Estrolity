f02b21024bf22756aeeae05f2505721bb8340851
asmi0803 <asmi.panchal05@gmail.com>
2024-03-20 23:39:45 +0530

    validation

---
+++ feedback3.js
@@ -0,0 +1,41 @@
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    var emailError = document.getElementById("emailError");
+    emailError.innerHTML = "";
+    if (!email) {
+        emailError.innerHTML = "Email is required";
+        return false;
+    } else if (!validateEmailFormat(email)) {
+        emailError.innerHTML = "Invalid email format";
+        return false;
+    }
+    return true;
+}
+
+function validateFeedback() {
+    var feedback = document.getElementById("feedback").value;
+    var feedbackError = document.getElementById("feedbackError");
+    feedbackError.innerHTML = "";
+    if (!feedback) {
+        feedbackError.innerHTML = "Feedback is required";
+        return false;
+    }
+    return true;
+}
+
+function validateRadio() {
+    var choiceError = document.getElementById("choiceError");
+    choiceError.innerHTML = "";
+    var yesChecked = document.getElementById("yes").checked;
+    var noChecked = document.getElementById("no").checked;
+    if (!yesChecked && !noChecked) {
+        choiceError.innerHTML = "Please select an option";
+        return false;
+    }
+    return true;
+}
+
+function validateEmailFormat(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
