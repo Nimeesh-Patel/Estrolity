714b2f68200c22d9e102a03000dccb76c7fb4dca
AbhinavNair0077 <159948606+AbhinavNair0077@users.noreply.github.com>
2024-03-21 03:42:28 +0530

    login valid

--- js/login_validation.js
+++ js/login_validation.js
@@ -75,24 +75,24 @@ function formValidation() {
 }
 
 // Function to generate captcha
-function generate() {
-    // Clear old input
-    document.getElementById("submit").value = "";
+// function generate() {
+//     // Clear old input
+//     document.getElementById("submit").value = "";
 
-    // Access the element to store the generated captcha
-    captcha = document.getElementById("image");
-    let uniquechar = "";
+//     // Access the element to store the generated captcha
+//     captcha = document.getElementById("image");
+//     let uniquechar = "";
 
-    const randomchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//     const randomchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    // Generate captcha for length of 5 with random character
-    for (let i = 1; i < 5; i++) {
-        uniquechar += randomchar.charAt(Math.random() * randomchar.length);
-    }
+//     // Generate captcha for length of 5 with random character
+//     for (let i = 1; i < 5; i++) {
+//         uniquechar += randomchar.charAt(Math.random() * randomchar.length);
+//     }
 
-    // Store generated input
-    captcha.innerHTML = uniquechar;
-}
+//     // Store generated input
+//     captcha.innerHTML = uniquechar;
+// }
 
 // let captcha;
 // var firstName = document.getElementById('user-input');
@@ -120,49 +120,53 @@ function generate() {
 //     return false;
 // }
 
-// function printmsg() {
-// 	const usr_input = document
-// 		.getElementById("submit").value;
+function printmsg() {
+	const usr_input = document
+		.getElementById("submit").value;
 
-// 	// Check whether the input is equal
-// 	// to generated captcha or not
-// 	if (usr_input == captcha.innerHTML) {
-// 		let s = document.getElementById("key")
-// 			.innerHTML = "Matched";
-//             window.location.href = 'login.html';
-// 		generate();
-// 	}
-// 	else {
-// 		let s = document.getElementById("key")
-// 			.innerHTML = "not Matched";
-//             alert("Captcha does not match",window.location.href='register.html');
-// 		generate();
-//         return false;
-// 	}
-// }
-// function generate() {
+	// Check whether the input is equal
+	// to generated captcha or not
+	if (usr_input == captcha.innerHTML) {
+		let s = document.getElementById("key")
+			.innerHTML = "Matched";
+            window.location.href = 'login.html';
+		generate();
+	}
+	else {
+		let s = document.getElementById("key")
+			.innerHTML = "not Matched";
+            alert("Captcha does not match",window.location.href='register.html');
+		generate();
+        return false;
+	}
+}
+function generate() {
 
-// 	// Clear old input
-// 	document.getElementById("submit").value = "";
+	// Clear old input
+	document.getElementById("submit").value = "";
 
-// 	// Access the element to store
-// 	// the generated captcha
-// 	captcha = document.getElementById("image");
-// 	let uniquechar = "";
+	// Access the element to store
+	// the generated captcha
+	captcha = document.getElementById("image");
+	let uniquechar = "";
 
-// 	const randomchar =
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const randomchar =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-// 	// Generate captcha for length of
-// 	// 5 with random character
-// 	for (let i = 1; i < 5; i++) {
-// 		uniquechar += randomchar.charAt(
-// 			Math.random() * randomchar.length)
-// 	}
+	// Generate captcha for length of
+	// 5 with random character
+	for (let i = 1; i < 5; i++) {
+		uniquechar += randomchar.charAt(
+			Math.random() * randomchar.length)
+	}
 
-// 	// Store generated input
-// 	captcha.innerHTML = uniquechar;
-//}
+	// Store generated input
+	captcha.innerHTML = uniquechar;
+}
+
+
+
+    
 // function userid_validation(uid, mx, my) {
 //     var uid_len = uid.value.length;
 //     if (uid_len == 0 || uid_len >= my || uid_len < mx) {
