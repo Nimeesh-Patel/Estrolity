201ba2bf1f9795463e4aef5f14adf354967f4c9d
Nimeesh-Patel <nimeeshpatel.no@gmail.com>
2024-03-21 12:50:29 +0530

    quiz 8 session not destroyed

--- quiz_8.php
+++ quiz_8.php
@@ -50,7 +50,7 @@ if (isset($_POST['socialInteraction8'])) {
     $conn->close();
 
     // Optionally, clear session variables after storing data
-    session_unset();
-    session_destroy();
+    // session_unset();
+    // session_destroy();
 }
 ?>
